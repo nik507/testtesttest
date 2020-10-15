5d43664c9e5f794455bfb9b69e995028d4bb0c16
Nikola <nik.7377@gmail.com>
2020-10-15 09:52:43 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+
+        int myVar = 25;
+        float f = 10.0f;
+        
+        Integer integer = new Integer(122);
+        System.out.println("Hello World" + f + myVar);
+    }
+}
